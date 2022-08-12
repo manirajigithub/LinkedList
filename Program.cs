6acb3d0189c630a34ec9e9d6412f15ffde6c7cab
mani1 <mani1@NANI143>
2022-08-12 20:58:29 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedListUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            linkedlist linklist = new linkedlist();
+            linklist.Add(70);
+            linklist.Add(30);
+            linklist.Add(56);
+            linklist.Display();
+        }
+    }
+}
+
