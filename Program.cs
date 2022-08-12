277d9e29a47b9fe66b61bde23c3c297b607f03e1
mani1 <mani1@NANI143>
2022-08-12 21:01:06 +0530

    [Rajeswari]Added node

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to LinkedList");
             linkedlist linklist = new linkedlist();
             linklist.Add(70);
             linklist.Add(30);
