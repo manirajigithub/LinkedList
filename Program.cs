bb170ca90e00bc42ca8208907ea236177167baa9
mani1 <mani1@NANI143>
2022-08-13 13:28:33 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedListUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            linkedlist linklist1 = new linkedlist();
+            linklist1.Add(56);
+            linklist1.Add(70);
+            linklist1.Display();
+            linklist.Insert(2, 30);
+        }
+    }
+}
+
