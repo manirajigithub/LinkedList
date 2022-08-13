6f92e490ee4264d0a853342077efed129cd56d84
mani1 <mani1@NANI143>
2022-08-13 13:45:43 +0530

    [Rajeswari]Added Delete Part

---
+++ LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUC5
+{
+    public class linkedlist
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted in Linkedlist", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("the list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("-------------------------------------------------------------------");
+        }
+
+        public Node Insert(int pos, int data)
+        {
+            Node newNode = new Node(data);
+            Node temp = head;
+            if (pos < 1)
+            {
+                Console.WriteLine("invalid ");
+            }
+            else if (pos == 1)
+            {
+                Node node = newNode;
+                node.next = temp;
+                head = node;
+                Console.WriteLine("The New data is : " + node.data + "Inserted");
+            }
+            else
+            {
+                int p = 1;
+                while (p != pos - 1)
+                {
+                    temp = temp.next;
+                    pos++;
+                }
+                Node node = newNode;
+                node.next = temp.next;
+                temp.next = node;
+                Console.WriteLine("The New data  " + node.data + "is Inserted");
+            }
+            return head;
+
+        }
+        internal void Pop()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Empty , Nothing to delete");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Popped Item is {0}", head.data);
+
+
+                Node temp = head;
+                head = head.next;
+                temp = head.next;
+                //head=temp.next; 
+            }
+        }
+    }
+}
