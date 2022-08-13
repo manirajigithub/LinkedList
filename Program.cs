1cc85ffb374b68a8f0d6671c97c723ed7ccf4c06
mani1 <mani1@NANI143>
2022-08-13 13:42:33 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedListUC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To DeleteFirst
+            linkedlist linklist1 = new linkedlist();
+            linklist1.Add(56);
+            linklist.Add(30);
+            linklist1.Add(70);
+            linklist1.Pop();
+            linklist1.Display();
+        }
+    }
+}
