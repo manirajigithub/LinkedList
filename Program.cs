5dbae6c2fdb8122eb760cee37518f2fc0dcfa00a
mani1 <mani1@NANI143>
2022-08-13 13:48:42 +0530

    [Rajeswari]Added Node in Delete Part

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             // To DeleteFirst
             linkedlist linklist1 = new linkedlist();
             linklist1.Add(56);
-            linklist.Add(30);
+            linklist1.Add(30);
             linklist1.Add(70);
             linklist1.Pop();
             linklist1.Display();
