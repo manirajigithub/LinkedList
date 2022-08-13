64d04a9d435553678cc6a3d2321e873fb82fe47e
mani1 <mani1@NANI143>
2022-08-13 20:07:36 +0530

    [Rajeswari]Added Node Part

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LinkedListUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            linkedlist linklist4 = new linkedlist();
+            linklist4.Add(56);
+            linklist4.Add(30);
+            linklist4.Add(70);
+            linklist4.Display();
+            linklist4.Find(30);
+        }
+    }
+}
