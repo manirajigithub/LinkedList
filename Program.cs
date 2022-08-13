9b4d25047229ec4c3dd2fd4b6fd2865f64f49b3f
mani1 <mani1@NANI143>
2022-08-13 13:22:27 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.DisplayQueue();
+        }
+    }
+}
