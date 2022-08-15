f4fa8119187ff4b369d7cc7e5ace23d969ee7e47
mani1 <mani1@NANI143>
2022-08-15 19:11:48 +0530

    [Rajeswari]Added UC9

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedListUC9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //To DeleteDataInBetween
+            linkedlist linklist6 = new linkedlist();
+            linklist6.Add(56);
+            linklist6.Add(30);
+            linklist6.Add(40);
+            linklist6.Add(70);
+            linklist6.DeleteBetween(40);
+            linklist6.Display();
+        }
+    }
+}
