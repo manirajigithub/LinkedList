b6428832ac72be7fc197f8445e8703fdf95048c3
mani1 <mani1@NANI143>
2022-08-15 19:19:36 +0530

    [Rajeswari]Added UC10

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedListUC9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //To Sorting
+            linkedlist linklist7 = new linkedlist();
+            linklist7.Add(56);
+            linklist7.Add(30);
+            linklist7.Add(40);
+            linklist7.Add(70);
+            linklist7.Sorting();
+            linklist7.Display();
+        }
+    }
+}
